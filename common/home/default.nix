111e1201a234ef50f36128d45ca97c0ad08133bd
Jack Diver <jackdiver@hotmail.co.uk>
2025-08-06 15:14:33 +0100

    Use catppuccin in bat

--- common/home/default.nix
+++ common/home/default.nix
@@ -8,7 +8,22 @@
 	];
 
 	programs = {
-		bat.enable = true;
+		bat = {
+			enable = true;
+			config.theme = "catppuccin-mocha";
+			themes = {
+				catppuccin-mocha = {
+					src =
+						pkgs.fetchFromGitHub {
+							owner = "catppuccin";
+							repo = "bat";
+							rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+							hash = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
+						};
+					file = "themes/Catppuccin Mocha.tmTheme";
+				};
+			};
+		};
 		eza = {
 			enable = true;
 			enableZshIntegration = true;
